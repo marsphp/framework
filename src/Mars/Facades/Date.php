477de8ec4d82828436be45371260bb1f950063bc
Hassane DAO <dao.hassane@gmail.com>
2024-12-19 16:52:30 +0000

    feat: add date facade and helper classes to framework

---
+++ src/Mars/Facades/Date.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mars\Facades;
+
+use Mars\Facades\Abstracts\Facade;
+
+class Date extends Facade {
+    public function getFacadeAccessor(): string
+    {
+        return 'date';
+    }
+}
---
+++ src/Mars/Helpers/Date.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mars\Helpers;
+
+use Mars\Facades\Abstracts\Facade;
+
+class Date extends Facade
+{
+
+}
